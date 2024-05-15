ad2702304b16b385f42b9a7d3533ed2d13b5cb53
DamiánD <deramodamian@gmail.com>
2024-05-15 18:32:47 -0300

    clases entidades

--- ProyectoTrnasversalEntrega1/src/proyectotrnasversalentrega1/ProyectoTrnasversalEntrega1.java
+++ ProyectoTrnasversalEntrega1/src/proyectotrnasversalentrega1/ProyectoTrnasversalEntrega1.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package universidadgrupo17;
+package proyectotrnasversalentrega1;
 
 /**
  *
- * @author Castro Maria Victoria
+ * @author Damián
  */
-public class UniversidadGrupo17 {
+public class ProyectoTrnasversalEntrega1 {
 
     /**
      * @param args the command line arguments
