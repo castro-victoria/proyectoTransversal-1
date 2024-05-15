568ea0e1ee5a1af75a2a580b337e3fb42f723d33
MarianelaRocamora <rocamoramarianela86@gmail.com>
2024-05-15 18:46:10 -0300

    Update ProyectoTrnasversalEntrega1.java

--- ProyectoTrnasversalEntrega1/src/proyectotrnasversalentrega1/ProyectoTrnasversalEntrega1.java
+++ ProyectoTrnasversalEntrega1/src/proyectotrnasversalentrega1/ProyectoTrnasversalEntrega1.java
@@ -16,6 +16,7 @@ public class ProyectoTrnasversalEntrega1 {
     public static void main(String[] args) {
         // TODO code application logic here
         //hola
+        //soy mari 
     }
     
 }
