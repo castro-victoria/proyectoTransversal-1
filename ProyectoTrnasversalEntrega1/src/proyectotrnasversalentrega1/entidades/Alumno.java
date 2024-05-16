6af2f57a57d408272e82b48bafbe0eb4b96ea286
DamiánD <deramodamian@gmail.com>
2024-05-16 19:39:41 -0300

    Se cambia nombre de package

--- ProyectoTrnasversalEntrega1/src/proyectotrnasversalentrega1/entidades/Alumno.java
+++ ProyectoTrnasversalEntrega1/src/proyectotrnasversalentrega1/entidades/Alumno.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package universidadgrupo17.entidades;
+package proyectotrnasversalentrega1.entidades;
 
 import java.time.LocalDate;
 
